a7d55ad2b2e3c657b1d7a07ba8547d8fc36f6a46
AlulCode45 <alulgans70@gmail.com>
2024-08-20 10:16:28 +0700

    [ADD] Added new migrations and controllers: 	new file:   app/Http/Controllers/AuthController.php 	new file:   app/Http/Middleware/AdminMiddleware.php 	modified:   database/migrations/0001_01_01_000000_create_users_table.php 	new file:   database/migrations/2024_08_20_020842_destination_company.php 	new file:   database/migrations/2024_08_20_020843_admin.php 	new file:   database/migrations/2024_08_20_020847_guests.php 	new file:   database/migrations/2024_08_20_020848_destinations.php 	new file:   database/migrations/2024_08_20_020859_testemonials.php 	new file:   database/migrations/2024_08_20_020911_ratings.php 	new file:   database/migrations/2024_08_20_020935_destination_images.php 	modified:   routes/web.php

---
+++ database/migrations/2024_08_20_020848_destinations.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('destinations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('province_id')->reference('id')->on('provincies');
+            $table->foreignId('regency_id')->reference('id')->on('regencies');
+            $table->foreignId('district_id')->reference('id')->on('districts');
+            $table->foreignId('village_id')->reference('id')->on('villages');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('destinations');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,4 +10,10 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard.Dashboard');
+});
+
+Route::prefix('/auth')->group(function () {
+    Route::get('/login', [AuthController::class, 'Login']);
+    Route::get('/register', [AuthController::class, 'Register']);
+    Route::get('/logout', [AuthController::class, 'Logout'])->middleware(AdminMiddleware::class)->name('logout');
 });
