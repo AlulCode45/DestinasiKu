e7355d962daa7e4b33246394a6d2229b7208f6cd
AlulCode45 <alulgans70@gmail.com>
2024-08-26 09:11:16 +0700

    [ADD] Search destination by name: 	modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
         <main class="mb-20">
             <div class="flex justify-between items-end">
                 <h1 class="font-bold text-2xl mt-10 w-96">Holiday accomodation recommendations for you</h1>
-                <div class="filter flex gap-4">
+                <form action="/destinations" method="GET" class="filter flex gap-4">
                     <div class="search relative">
                         <svg fill="#000" width="15px" height="15px" viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg"
@@ -103,10 +103,11 @@
                             <path
                                 d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z" />
                         </svg>
-                        <input type="text" placeholder="Search destions" class="px-12 rounded-full py-3 border">
+                        <input type="text" placeholder="Search destions" class="px-12 rounded-full py-3 border"
+                            name="name">
                     </div>
-                    <button class="bg-black px-5 py-1 rounded-full text-white">Search</button>
-                </div>
+                    <button class="bg-black px-5 py-1 rounded-full text-white" type="submit">Search</button>
+                </form>
             </div>
 
             <div class="grid grid-cols-3 gap-7 mt-5">
